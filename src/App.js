164246e38c2ddbfb91400a36cf36d08a3dfe2eae
Sophie Deskin <sophiedeskin@gmail.com>
2021-10-24 10:11:15 -0700

    setting up initial files and testing tailwindcss

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+
+function App() {
+  return (
+<header>
+  <nav>
+    <div class="">
+      <div class="flex justify-between h-16 px-10 shadow items-center">
+        <div class="flex items-center space-x-8">
+          <h1 class="text-xl lg:text-2xl font-bold cursor-pointer">Tailwind</h1>
+          <div class="hidden md:flex justify-end space-x-4">
+            <a href="#" class="hover:text-indigo-600 text-gray-700">Hello</a>
+            <a href="#" class="hover:text-indigo-600 text-gray-700">About</a>
+            <a href="#" class="hover:text-indigo-600 text-gray-700">Service</a>
+            <a href="#" class="hover:text-indigo-600 text-gray-700">Contact</a>
+          </div>
+        </div>
+    </div>
+  </div>
+</nav>
+    </header>
+  );
+}
+
+export default App;
